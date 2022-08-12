e75bd0440bcf7b070f203f93f19e09af5cedca05
emirvur <58534335+emirvur@users.noreply.github.com>
2022-08-12 18:49:40 +0300

    Create colors.dart

---
+++ constants/colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const Color kBackground = Color(0xFFF5F5F5);
+const Color kPrimaryColor = Color(0xFFF49333); //Color(0xFFFDB730);
+const Color sarirenk = Color(0xFFFDBE36);
+const Color tasarimbilgiyesil = Color(0xFF78E13D);
+const Color dikimbilgikirmizi = Color(0xFFFDBE36);
+const Color cirtlaksari = Color(0xFFFFC107);
+const Color checkboxgrisi = Color(0xFFC7C7C7);
+const Color aragrisi = Color(0xFFE3E3E3);
+const Color golge = Color(0xFFB5C1C9);
+const Color miracgolge = Color(0xFFF4F6F7);
+const Color whatsappyesil = Color(0xffDCF8C6);
+const Color anayazi = Color(0xFF4B515C);
+const Color girmsiarkaplan = Color(0xFFF2F2F2);
+const Color welcomegri = Color(0xFF979797);
+const Color yetkinliklergrisi = Color(0xFF4B515C);
+const Color tamamlandiyesili = Color(0xFF008000);
+const MaterialColor themdatamaincolor = const MaterialColor(
+  0xFFF49333,
+  const <int, Color>{
+    50: const Color(0xFF0E7AC7),
+    100: const Color(0xFF0E7AC7),
+    200: const Color(0xFF0E7AC7),
+    300: const Color(0xFF0E7AC7),
+    400: const Color(0xFF0E7AC7),
+    500: const Color(0xFF0E7AC7),
+    600: const Color(0xFF0E7AC7),
+    700: const Color(0xFF0E7AC7),
+    800: const Color(0xFF0E7AC7),
+    900: const Color(0xFF0E7AC7),
+  },
+);
