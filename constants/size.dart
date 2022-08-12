75be7a9663ef34005966cfb85b1ea8a114846019
emirvur <58534335+emirvur@users.noreply.github.com>
2022-08-12 18:50:39 +0300

    Create size.dart

---
+++ constants/size.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppSize {
+  static const double iconsize = 20;
+  static const double grandeIconsize = 24;
+  static const double ultraIconsize = 28;
+  static const double avatarRadius = 20;
+  static const double miniavatarRadius = 15;
+  static const double hintsize = 12;
+  static const double normalsize = 15;
+  static const double smallsize = 12;
+  static const double verticalPadding = 8;
+  static const double horizontalPadding = 15;
+  static const double paddingAll = 8;
+}
